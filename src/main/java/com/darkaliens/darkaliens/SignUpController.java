8c148d83d6d3c254098894cc723127129911308d
Pankaj kumar Shah <pankaj123.pks@gmail.com>
2022-04-03 13:59:43 +0200

    Created Sign up page

--- src/main/java/com/darkaliens/darkaliens/SignUpController.java
+++ src/main/java/com/darkaliens/darkaliens/SignUpController.java
@@ -1,7 +1,11 @@
 package com.darkaliens.darkaliens;
 
 public class SignUpController {
-    public void goBackToLogin() {
+    public void onSignupButtonClick() {
+        StageManager.setScene("home-view.fxml","Search Flight");
+    }
+
+    public void onLoginButtonClick() {
         StageManager.goToLoginScene();
     }
 }
